d3d85cdfe905642200cb197a7b1ab55a1de88e20
Ivan Pereiro <ivanpereariza@gmail.com>
2023-02-03 11:19:59 +0100

    enter

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ const img = document.querySelector('img')
 const canvas = document.querySelector('canvas')
 
 
-document.addEventListener('click', () => {
-    img.classList.add('d-none')
-    canvas.classList.remove('d-none')
-    controlerApp.init()
+document.addEventListener('keydown', event => {
+    if (event.code === "Enter") {
+        img.classList.add('d-none')
+        canvas.classList.remove('d-none')
+        controlerApp.init()
+    }
 })
 
 
